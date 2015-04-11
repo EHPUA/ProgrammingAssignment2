b1ec365a7ece708dd9084f95afab050ede4924fb
Edward Phua <edwardphua@ymail.com>
2015-04-11 22:21:13 +0800

    Create complete.R

---
+++ complete.R
@@ -0,0 +1,18 @@
+complete  <- function(directory, id = 1:332) {
+        # loading library
+        library (data.table)
+        # getting list of file
+        lsFileFull <- list.files (directory, full.names = TRUE)
+        #creates an empty data frame for cbind & rbind
+        dat <- data.frame() 
+        dat2 <- data.frame() 
+        # Looping 
+        for (i in id) {                                
+                dat <- fread(lsFileFull[i])
+                x <- sum(complete.cases(dat))
+                dat <- data.frame(cbind(id=i,nobs=x)) 
+                dat2 <- data.frame(rbind(dat2,dat))
+        }
+        # return result 
+        return (dat2)
+}
